c3fc2b427a84e7938a7d0f8dd870be6eb1946eda
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2025-09-11 10:02:17 -0400

    asynchronized user controller methods, fixed exception bug with GET username

--- server/src/controllers/user/user.controller.ts
+++ server/src/controllers/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query } from '@nestjs/common';
 import type { User } from 'src/models/user.entity';
 import { UserService } from 'src/services/user/user.service';
 import { PublicUserDto } from './dto/public-user.dto';
@@ -9,29 +9,29 @@ export class UserController {
     constructor(private readonly userService: UserService){}
 
     @Get()
-    getUsers(){
-        return this.userService.getUsers();
+    async getUsers(){
+        return await this.userService.getUsers();
     }
 
     @Post()
-    addUser(@Body() user: User){
-        return this.userService.addUser(user);
+    async addUser(@Body() user: User){
+        return await this.userService.addUser(user);
     }
 
     @Put(":id")
-    updateUser(@Param("id") userId: number, @Body() user){
-        return this.userService.updateUser(userId, user);
+    async updateUser(@Param("id") userId: number, @Body() user){
+        return await this.userService.updateUser(userId, user);
     }
 
     @Delete(":id")
-    deleteUser(@Param("id") userId){
-        return this.userService.deleteUser(userId);
+    async deleteUser(@Param("id") userId){
+        return await this.userService.deleteUser(userId);
     }
 
     @Get(":username")
     async findUser(@Param("username") username:string){
         let user = await this.userService.findByUsername(username);
-        if(!user) return { message: 'User does not exist' };
+        if(!user) throw new NotFoundException('User does not exist');
 
         return new PublicUserDto(user);
     }
