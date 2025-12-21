e34d601313d5b6e75bb8cd1aebb850906d4d19db
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2025-12-21 16:22:17 -0500

    Secured application dashboard viewing
    User is now redirected to login and will not be able to view the dashboard without logging in

--- web-client/react-client/src/App.jsx
+++ web-client/react-client/src/App.jsx
@@ -11,23 +11,56 @@ import PersonalJarPage from './pages/PersonalJarPage';
 import AddExpensePage from './pages/AddExpensePage';
 import AddPaymentPage from './pages/AddPaymentPage';
 import SettleUpPage from './pages/SettleUpPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/create-hive" element={<CreateHivePage />} />
-          <Route path="/hive/:id" element={<HiveDetailPage />} />
-          <Route path="/add-transaction" element={<AddTransactionPage />} />
-          <Route path="/view-transactions" element={<PersonalJarPage />} />
-          <Route path="/hive/:hiveId/add-expense" element={<AddExpensePage />} />
-          <Route path="/hive/:hiveId/add-payment" element={<AddPaymentPage />} />
-          <Route path="/hive/:id/settle-up" element={<SettleUpPage />} />
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/create-hive" element={
+            <ProtectedRoute>
+              <CreateHivePage />
+            </ProtectedRoute>
+          } />
+          <Route path="/hive/:id" element={
+            <ProtectedRoute>
+              <HiveDetailPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/add-transaction" element={
+            <ProtectedRoute>
+              <AddTransactionPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/view-transactions" element={
+            <ProtectedRoute>
+              <PersonalJarPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/hive/:hiveId/add-expense" element={
+            <ProtectedRoute>
+              <AddExpensePage />
+            </ProtectedRoute>
+          } />
+          <Route path="/hive/:hiveId/add-payment" element={
+            <ProtectedRoute>
+              <AddPaymentPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/hive/:id/settle-up" element={
+            <ProtectedRoute>
+              <SettleUpPage />
+            </ProtectedRoute>
+          } />
         </Routes>
       </Layout>
     </Router>
