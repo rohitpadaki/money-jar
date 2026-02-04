788d86da73f2b498493858a7feb11f4469d62c60
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2026-02-04 17:27:18 -0500

    initialized java backend

---
+++ server-java/moneyjar/moneyjar/src/main/java/com/tarungattu/moneyjar/MoneyjarApplication.java
@@ -0,0 +1,13 @@
+package com.tarungattu.moneyjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyjarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyjarApplication.class, args);
+	}
+
+}
---
+++ server-java/moneyjar/moneyjar/src/test/java/com/tarungattu/moneyjar/MoneyjarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarungattu.moneyjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyjarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
